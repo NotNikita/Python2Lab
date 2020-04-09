99c7a7c0a10438593a8eb7f113e60ba1a949a819
Nikitok <nikitabsuir@gmail.com>
2020-04-09 12:26:52 +0300

    test for kwargs added

--- utest_task4.py
+++ utest_task4.py
@@ -1,17 +1,19 @@
-import task4
+from task4 import func
 import unittest
 
 
 class DecoratorTest(unittest.TestCase):
 
-    def test_f(self):
-        self.assertNotEqual(task4.f(4), 0)
+    def test_f(self): # testing kwargs
+        func(Hello=100, World = 200)
+        self.assertNotEqual(func('Hello'), 100)
 
-    def test_f2(self):
-        self.assertEqual(task4.f(5), task4.f(5))
+    def test_f2(self): #testing memorizing
+        self.assertEqual(func(5), func(5))
 
-    def test_f3(self):
-        self.assertNotEqual(task4.f(5), task4.f(2))
+    def test_f3(self): #testing args
+        func(1,5,10,15,20)
+        self.assertNotEqual(func(5), func(10))
 
 
 if __name__ == '__main__':
