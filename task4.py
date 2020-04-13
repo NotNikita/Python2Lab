15dab74b8d895267ebd4f91d5b0b2512d148620f
Nikitok <nikitabsuir@gmail.com>
2020-04-13 20:55:40 +0300

    Final?

--- task4.py
+++ task4.py
@@ -5,23 +5,39 @@ def cached(function):
     memo = {}  # like memory
 
     def wrapper(*args, **kwargs):
-        out_arr = {} # output dictionary
+        out_arr = []  # output dictionary
+        temp_arr = [] # for kwargs
 
-        for num in args: # for each arg in *args
-            if num in memo:
-                    out_arr[num] = memo[num]
+        #args
+        assert isinstance(args, tuple) # args to tuple
+        if args.__len__() != 0:
+            if args in memo:
+                out_arr = memo[args]
             else:
-                    value_for_num = function(num)
-                    out_arr[num] = value_for_num
-                    memo[num] = value_for_num
+                for x in args:
+                    out_arr.append(function(x))
+                memo[args] = out_arr
 
-        for word in kwargs.keys():
-            if word in memo:
-                out_arr[word] = memo[word]
+        # kwargs
+        if kwargs.__len__() !=0:  # если словарь вообще был передан
+            temp = list(kwargs.items())  # делаем список из элементов словоря
+            kwarg_content = tuple(temp)  # кортеж
+
+
+            if kwarg_content in memo:
+                temp_arr = memo[kwarg_content]
+                for i in temp_arr:  # merging lists of arg and kwarg results
+                    out_arr.append(i)
             else:
-                value_for_num = kwargs[word] # how to get second value??
-                out_arr[word] = value_for_num
-                memo[word] = value_for_num
+                for key in kwargs.keys(): #выписываем значения по ключам в список для вывода
+                    temp_arr.append(kwargs[key])
+
+                memo[kwarg_content] = temp_arr  # 'кортеж' = значения элементов кортежа
+
+                if len(temp_arr) != 0: # kwargs were not given
+                    for i in temp_arr:  # merging lists of arg and kwarg results
+                        out_arr.append(i)
+
         return out_arr
 
     return wrapper
@@ -32,7 +48,7 @@ def func(arg):
     return random.randint(1, 100) * arg
 
 
+print("result: ", func(1, 2, 3) == func(1, 2, 3))  # позиционные аргументы
+print("\n")
+print("result: ", func(Hi=100, Me=200) == func(Hi=100, Me=200))
 
-func(1,2,3, Hi = 100, Me = 200)  # позиционные аргументы
-func(1,2)
-func('Hi')
--- utest_task4.py
+++ utest_task4.py
@@ -6,14 +6,13 @@ class DecoratorTest(unittest.TestCase):
 
     def test_f(self): # testing kwargs
         func(Hello=100, World = 200)
-        self.assertNotEqual(func('Hello'), 100)
+        self.assertEqual(func(Hello=100, World = 200), func(Hello=100, World = 200))
 
     def test_f2(self): #testing memorizing
-        self.assertEqual(func(5), func(5))
+        self.assertEqual(func(5,2,3,4), func(5,2,3,4))
 
     def test_f3(self): #testing args
-        func(1,5,10,15,20)
-        self.assertNotEqual(func(5), func(10))
+        self.assertEqual(func(5, Hello_Dolly = 1, Hello_World =2), func(5, Hello_Dolly = 1, Hello_World =2))
 
 
 if __name__ == '__main__':
