f4af5ec67077dcf8c8a069e8a8996460c9831983
Nikitok <nikitabsuir@gmail.com>
2020-04-16 21:06:39 +0300

    Huge changes

--- task4.py
+++ task4.py
@@ -5,50 +5,46 @@ def cached(function):
     memo = {}  # like memory
 
     def wrapper(*args, **kwargs):
-        out_arr = []  # output dictionary
-        temp_arr = [] # for kwargs
+        # key - input
+        # result - output
+        key = args
+        if kwargs:
+            for item in kwargs.items():
+                key += item
 
-        #args
-        assert isinstance(args, tuple) # args to tuple
-        if args.__len__() != 0:
-            if args in memo:
-                out_arr = memo[args]
-            else:
-                for x in args:
-                    out_arr.append(function(x))
-                memo[args] = out_arr
+        # ==
+        if key in memo:
+            result = memo[key]
+        else:
+            result = function(*args, **kwargs)
+            memo[key] = result
 
-        # kwargs
-        if kwargs.__len__() !=0:  # если словарь вообще был передан
-            temp = list(kwargs.items())  # делаем список из элементов словоря
-            kwarg_content = tuple(temp)  # кортеж
-
-
-            if kwarg_content in memo:
-                temp_arr = memo[kwarg_content]
-                for i in temp_arr:  # merging lists of arg and kwarg results
-                    out_arr.append(i)
-            else:
-                for key in kwargs.keys(): #выписываем значения по ключам в список для вывода
-                    temp_arr.append(kwargs[key])
-
-                memo[kwarg_content] = temp_arr  # 'кортеж' = значения элементов кортежа
-
-                if len(temp_arr) != 0: # kwargs were not given
-                    for i in temp_arr:  # merging lists of arg and kwarg results
-                        out_arr.append(i)
-
-        return out_arr
+        return result
 
     return wrapper
 
 
 @cached
-def func(arg):
-    return random.randint(1, 100) * arg
+def func(*args, **kwargs):
+    out_arr = []  # output dictionary
+    temp_arr = []  # for kwargs
+
+    # args
+    if args.__len__() != 0:# если args вообще был передан
+        for n in args:
+            out_arr.append(random.randint(1, 100) * n)
+
+    # kwargs
+    if kwargs.__len__() != 0:  # если kwargs вообще был передан
+        for key in kwargs.keys():  # выписываем значения по ключам в список для вывода
+            temp_arr.append(kwargs[key])
+
+        for i in temp_arr:  # merging lists of arg and kwarg results
+            out_arr.append(i)
+
+    return out_arr
 
 
 print("result: ", func(1, 2, 3) == func(1, 2, 3))  # позиционные аргументы
 print("\n")
 print("result: ", func(Hi=100, Me=200) == func(Hi=100, Me=200))
-
